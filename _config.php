7fcc849d20d61edba188849f4920e037c7e14e04
James Cocker <git@pswd.biz>
2013-10-28 12:55:12 +0000

    Config: Moved items from PHP to YML

--- _config.php
+++ _config.php
@@ -11,18 +11,12 @@ if (class_exists('CustomHtmlEditorConfig')) {
 	$simpleConfig->setOption('friendly_name', 'Simple WYSIWYG');
 	$simpleConfig->setOption('width', '60%');
 	$simpleConfig->setOption('theme_advanced_statusbar_location', 'none');
-	$simpleConfig->setButtonsForLine(1, array("bold","italic",'formatselect','bulist','numlist','outdent','indent','pastetext','ssmedia','sslink','unlink','code'));
+	$simpleConfig->setButtonsForLine(1, array("bold","italic",'formatselect','bullist','numlist','outdent','indent','pastetext','ssmedia','sslink','unlink','code'));
 	$simpleConfig->setButtonsForLine(2, array());
 	$simpleConfig->setButtonsForLine(3, array());
 	$simpleConfig->setOption('theme_advanced_disable', "sub,sup");
 	$simpleConfig->setOption('theme_advanced_blockformats', 'p,Heading=h3');
 }
-
-// Set JPG Quality
-GD::set_default_quality(90);
-
-// Custom CSS for CMS
-LeftAndMain::require_css('mypswd-tweaks/css/cms.css');
 
 // Remove any extensions via the mysite/_config.php, eg.:
 // Page::remove_extension('PageHideExtraMetaData');

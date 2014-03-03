aa71ff401630bcd60bfe548b4de418e17a8900e5
James Cocker <git@pswd.biz>
2014-03-03 11:53:57 +0000

    CMSHelpLink: Opens in new window/tab

--- code/extensions/CMSHelpLink.php
+++ code/extensions/CMSHelpLink.php
@@ -4,7 +4,7 @@ class CMSHelpLink extends DataExtension {
 	
 	function init() {
 		CMSMenu::remove_menu_item('Help');
-		CMSMenu::add_link('Help',"Help","http://www.silverstripe.purplespider.com/".Config::inst()->get($this->class, 'url'),-2);
+		CMSMenu::add_link('Help',"Help","http://www.silverstripe.purplespider.com/".Config::inst()->get($this->class, 'url'),-2,array("target"=>"_blank"));
 	}
 	
 }

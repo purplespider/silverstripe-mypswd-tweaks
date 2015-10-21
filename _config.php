26083ae8c9d478a8fe8929e8b7341fe3bf1620cd
James Cocker <git@pswd.biz>
2015-10-21 17:47:45 +0100

    TinyMCE Option Tweaks
    Disable paste as plain text by default, adds 'valid_styles' and cleanup rules instead.

--- _config.php
+++ _config.php
@@ -1,9 +1,25 @@
 <?php
 
 // TinyMCE Tweaks
-HtmlEditorConfig::get('cms')->setOption('paste_text_sticky', 'true');
-HtmlEditorConfig::get('cms')->setOption('paste_text_sticky_default', 'true');
 HtmlEditorConfig::get('cms')->setOption('theme_advanced_disable', 'underline,justifyfull,pasteword,spellchecker');
+HtmlEditorConfig::get('cms')->setOption('valid_styles', array('*' => 'text-align'));
+HtmlEditorConfig::get('cms')// Add support for HTML5 elements in tinymce editor
+    ->setOption('extended_valid_elements',
+        '+article,aside,audio[src|preload<none?metadata?auto|autoplay<autoplay|loop<loop|controls<controls|mediagroup],canvas[width,height],'
+        .'datalist[data],details[open<open],eventsource[src],fieldset[disabled<disabled|form|name],header,mark,menu[type<context?toolbar?list|label],'
+        .'meter[value|min|low|high|max|optimum],nav,progress[value,max],script[src|async<async|defer<defer|type|charset],section,time[datetime],'
+        .'video[preload<none?metadata?auto|src|crossorigin|poster|autoplay<autoplay|mediagroup|loop<loop|muted<muted|controls<controls|width|height],wbr,#span,'
+        .'form[id|method|onsubmit|onreset|action],input[id|name|style|type|placeholder],label[for]'
+    )
+    ->setOptions(array(//clean up the actions upon pasting text in
+        'paste_remove_spans'            => true,
+        'paste_remove_styles'           => true,
+        'paste_remove_styles_if_webkit' => true,
+        'force_br_newlines'             => true,
+        'force_p_newlines'              => false,
+        'paste_text_linebreaktype'      => "br",
+        'paste_auto_cleanup_on_paste'   => true,
+    ));
 
 // Create Simple HTMLEditor Config for use with nathancox/customhtmleditorfield
 if (class_exists('CustomHtmlEditorConfig')) {
@@ -19,7 +35,9 @@ if (class_exists('CustomHtmlEditorConfig')) {
 }
 
 // Changes Image backend to GD (required for SilverStripe Optimised Image module)
-Image::set_backend("OptimisedGDBackend");
+if (class_exists('OptimisedGDBackend')) { 
+	Image::set_backend("OptimisedGDBackend");
+}
 
 // Remove any extensions via the mysite/_config.php, eg.:
 // Page::remove_extension('PageHideExtraMetaData');

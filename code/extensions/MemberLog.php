e28eaa0e58fdfa8e4d24b938cf6242c0623685dc
James Cocker <git@pswd.biz>
2019-09-09 22:23:10 +0100

    Added Member Logging (LastVisited & NumVisit)
    From: https://docs.silverstripe.org/en/4/developer_guides/extending/how_tos/track_member_logins/

---
+++ code/extensions/MemberLog.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace PurpleSpider\SSTweaks;
+
+use SilverStripe\Forms\ReadonlyField;
+use SilverStripe\Security\Security;
+use SilverStripe\ORM\DB;
+use SilverStripe\ORM\DataExtension;
+
+class MemberLog extends DataExtension
+{
+    private static $db = [
+        'LastVisited' => 'Datetime',
+        'NumVisit' => 'Int',
+    ];
+
+    public function afterMemberLoggedIn()
+    {
+        $this->logVisit();
+    }
+
+    public function memberAutoLoggedIn()
+    {
+        $this->logVisit();
+    }
+
+    public function updateCMSFields(FieldList $fields)
+    {
+        $fields->addFieldsToTab('Root.Main', [
+            ReadonlyField::create('LastVisited', 'Last visited'),
+            ReadonlyField::create('NumVisit', 'Number of visits')
+        ]);
+    }
+
+    protected function logVisit()
+    {
+        if (!Security::database_is_ready()) return;
+
+        DB::query(sprintf(
+            'UPDATE "Member" SET "LastVisited" = %s, "NumVisit" = "NumVisit" + 1 WHERE "ID" = %d',
+            DB::get_conn()->now(),
+            $this->owner->ID
+        ));
+    }
+}
